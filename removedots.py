71644596d1c44150dfb8b3de81e5e2704393e654
Marcelo Larios <marcelolarios@users.noreply.github.com>
2023-12-16 21:54:29 -0300

    Add files via upload
    atualiza na pasta corrente e em subfolders

--- removedots.py
+++ removedots.py
@@ -2,17 +2,20 @@
 cd /filespath/
 python3 tiraponto.py
 '''
-import glob
 import os
+import glob
 
 def filename_replacer(filename):
-  fname, fext = os.path.splitext(filename)
-  return fname.replace(".", " ") + fext
+	fname, fext = os.path.splitext(filename)
+	return fname.replace(".", " ") + fext
   
 print('\nFiles found:')
-for name in glob.glob('*.*.*'): #dots in filename
-	troca = filename_replacer(name)
-	print(name)
-	os.rename(name, troca)
+
+for root, _, files in os.walk(os.getcwd()):
+	os.chdir(os.path.join(root))
+	for name in glob.glob('*.*.*'): #dots in filename
+		troca = filename_replacer(name)
+		print(name)
+		os.rename(name, troca)
 
 print('\nDone!')
